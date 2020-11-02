b8f4dddc860f9e3784308235c44463d7b9be4279
LetYou <simon200093@163.com>
2020-11-03 00:04:16 +0800

    chat

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../services/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
-  constructor() { }
+  input="";
+  messages: any;
+
+  constructor(
+    private chat: ChatService,
+
+  ) {    
+
+}
 
   ngOnInit() {
+    this.chat.startChat("0001").then(data => {
+      this.chat.getMessages().subscribe(msg => {
+        this.messages = msg;
+        console.log(this.messages)
+      }) 
+    });
+
+    
+
+  }
+
+  clickSend(){
+    if(this.input==""){
+      this.chat.presentToast("Message cannot be empty")
+    }else{
+      this.chat.sendMessage(this.input).catch(error => {if(error) this.chat.presentToast("fail to send")}).then(doc => { this.input=""})
+    }
   }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { ToastController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 
@@ -8,5 +9,51 @@ import { Observable } from 'rxjs';
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(
+    private fire: AngularFirestore,
+    private toast: ToastController,
+  ) { }
+
+  async presentToast(msg) {
+    const toast = await this.toast.create({
+      message: msg,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+  getMessages(){
+    return this.fire.collection("chat/" + localStorage.getItem("roomId") + "/messages", ref => ref.orderBy("time", "asc")).valueChanges()
+  }
+
+  public startChat(staff) {
+    return this.fire.collection("chat").add({
+      caller_id: localStorage.getItem("email"),
+      staff: staff,
+      time: new Date()
+    }).then(docRef => {
+      localStorage.setItem("roomId",docRef.id)
+      this.fire.collection("chat").doc(docRef.id).collection("messages").add({
+        reply: true,
+        content: "Hi there, how can I help?",
+        time: new Date()
+      }).catch(error => {
+        if (error) { this.presentToast("failed to connect, please try again later") }
+      }).then(data => {
+         this.fire.collection("tech").doc("staff").update({ava:"false"})
+      })
+    })
+  }
+
+  sendMessage(content){
+    return this.fire.collection("chat/" + localStorage.getItem("roomId")+ "/messages").add({
+      reply: false,
+      content: content,
+      time: new Date()
+    })
+  }
+
+
+
+
 }
