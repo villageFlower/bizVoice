0388827465ead433c02825cadcd4c300930b7e56
zbyNokin <dav1dzhangby@gmail.com>
2020-11-04 18:31:22 +0800

    烂活

--- src/app/qna-detail/qna-detail.page.ts
+++ src/app/qna-detail/qna-detail.page.ts
@@ -34,7 +34,7 @@ export class QnaDetailPage implements OnInit {
     const modal = await this.modalController.create({
       component: MyModalPage,
       componentProps: {
-        
+        "qnaID": localStorage.getItem("qnaID"),
       }
     });
 
